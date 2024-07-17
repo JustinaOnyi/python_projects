ef2d166b9ae4b8b01fd84c99894110ef55096f05
Justina Roland <justina.roland@phed.com.ng>
2024-07-17 16:13:34 +0100

    added number guesser game too

---
+++ number_guesser.py
@@ -0,0 +1,36 @@
+import random
+
+
+# # Displays a range of random nos excluding 11
+# r = random.randrange(-5, 11)
+ 
+top_of_range = input("Type a number: ")
+
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+
+    if top_of_range <= 0:
+        print('Please type a number larger than 0 next time.')
+        quit()
+    else:
+        print('Please type a number next time.')
+        quit()
+
+    
+# Displays range of random nos including 11
+random_number = random.randint(0, top_of_range)
+# print(random_number)
+
+while True:
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print('Please type a number next time.')
+        continue
+    if user_guess == random_number:
+        print("You got it!")
+        break
+    else:
+        print("You got it wrong!")
+
