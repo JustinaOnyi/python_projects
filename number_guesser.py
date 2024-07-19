ed4f83dea09eb2214fc6c02c10356249eb7baa3b
Justina Roland <justina.roland@phed.com.ng>
2024-07-19 11:47:08 +0100

    finished up with number guesser

--- number_guesser.py
+++ number_guesser.py
@@ -12,16 +12,18 @@ if top_of_range.isdigit():
     if top_of_range <= 0:
         print('Please type a number larger than 0 next time.')
         quit()
-    else:
-        print('Please type a number next time.')
-        quit()
+else:
+    print('Please type a number next time.')
+    quit()
 
     
 # Displays range of random nos including 11
 random_number = random.randint(0, top_of_range)
 # print(random_number)
+guesses = 0
 
 while True:
+    guesses += 1
     user_guess = input("Make a guess: ")
     if user_guess.isdigit():
         user_guess = int(user_guess)
@@ -32,5 +34,10 @@ while True:
         print("You got it!")
         break
     else:
-        print("You got it wrong!")
+        # print("You got it wrong!")
+        if user_guess > random_number:
+            print("You were above the number!")
+        else:
+            print("You were below the number!")
 
+print("You got it in", guesses, "guesses")
